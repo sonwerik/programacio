59edc63a1c9d5ffe5269017b4740577353a00dc6
sonwerik <sonwerik@gmail.com>
2024-10-23 10:20:54 +0200

    miau

---
+++ ProgramacioII/src/SequencialsCondicionalsIteratius/Iteratius/pt19Capicua.java
@@ -0,0 +1,13 @@
+package SequencialsCondicionalsIteratius.Iteratius;
+
+import java.util.Scanner;
+
+public class pt19Capicua {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(n);
+    }
+}
---
+++ ProgramacioII/src/SequencialsCondicionalsIteratius/Seqüencials/pt8WifiRadi.java
@@ -0,0 +1,16 @@
+package SequencialsCondicionalsIteratius.Seqüencials;
+
+import java.util.Scanner;
+
+public class pt8WifiRadi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        float area = scanner.nextFloat();
+        float radius = (float) Math.sqrt(area / Math.PI);
+        int distance = Math.round(radius);
+
+        System.out.println(distance);
+
+    }
+}
